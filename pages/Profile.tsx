64334d9d5717900ff15b9062ec691fe0ae1c5146
Mohamed Ibnahmad <ibnahmadmohamed8@gmail.com>
2022-07-10 13:10:21 +0100

    cookies config

---
+++ pages/Profile.tsx
@@ -0,0 +1,55 @@
+import { useKeycloak } from '@react-keycloak/ssr'
+import type { KeycloakInstance, KeycloakTokenParsed } from 'keycloak-js'
+import type { NextPage } from 'next'
+import * as React from 'react'
+
+import { Layout } from '../components/Layout'
+
+type ParsedToken = KeycloakTokenParsed & {
+  email?: string
+
+  preferred_username?: string
+
+  given_name?: string
+
+  family_name?: string
+}
+
+const ProfilePage: NextPage = () => {
+  const { keycloak } = useKeycloak<KeycloakInstance>()
+  const parsedToken: ParsedToken | undefined = keycloak?.tokenParsed
+
+  const profile = keycloak?.authenticated ? (
+    <ul>
+      <li>
+        <span className="font-weight-bold mr-1">Email:</span>
+        <span className="text-muted">{parsedToken?.email ?? ''}</span>
+      </li>
+      <li>
+        <span className="font-weight-bold mr-1">Username:</span>
+        <span className="text-muted">
+          {parsedToken?.preferred_username ?? ''}
+        </span>
+      </li>
+      <li>
+        <span className="font-weight-bold mr-1">First Name:</span>
+        <span className="text-muted">{parsedToken?.given_name ?? ''}</span>
+      </li>
+      <li>
+        <span className="font-weight-bold mr-1">Last Name:</span>
+        <span className="text-muted">{parsedToken?.family_name ?? ''}</span>
+      </li>
+    </ul>
+  ) : (
+    <span>Please login to view profile.</span>
+  )
+
+  return (
+    <Layout title="Profile | Next.js + Keycloak Example">
+      <h1 className="my-5">User Profile</h1>
+      {profile}
+    </Layout>
+  )
+}
+
+export default ProfilePage
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,9 @@ import '../styles/index.css'
 import { ReactKeycloakProvider } from '@react-keycloak/web'
 import cookie from 'cookie'
 import * as React from 'react'
-import type { IncomingMessage } from 'http'
-import type { AppProps, AppContext } from 'next/app'
-import { SSRKeycloakProvider, SSRCookies } from '@react-keycloak/ssr'
-
+import type { IncomingMessage } from 'http' //  i used this to get the cookies from the request
+import type { AppProps, AppContext } from 'next/app' // i use app
+import { SSRKeycloakProvider, SSRCookies } from '@react-keycloak/ssr' // provides SSR context for keycloak because it's not available in the client
 const KeycloakConfig = {
   realm: "fm6",// name of the realm
   url: "https://keycloaktest.ayouris.net/auth",// url of the Keycloak server
@@ -16,13 +15,48 @@ interface InitialProps {
   cookies: unknown
 }
 function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
-
-
   return (
-    <SSRKeycloakProvider
+    <SSRKeycloakProvider    
       keycloakConfig={KeycloakConfig}
       persistor={SSRCookies(cookies)}
-      
+      init={true}
+      initOnSSR={true}
+      initOptions={{
+        onLoad: 'check-state',
+        checkLoginIframe: false,
+        checkLoginIframeInterval: 5,
+        token: cookies.token,
+        refreshToken: cookies.refreshToken,
+        idToken: cookies.idToken,
+        expiresIn: cookies.expiresIn,
+        scope: cookies.scope,
+        timeStamp: cookies.timeStamp,
+        nonce: cookies.nonce,
+        state: cookies.state,
+        code: cookies.code,
+        redirectUri: cookies.redirectUri,
+        responseMode: cookies.responseMode,
+        responseType: cookies.responseType,
+        flow: cookies.flow,
+        display: cookies.display,
+        locale: cookies.locale,
+        action: cookies.action,
+        uid: cookies.uid,
+        username: cookies.username,
+        email: cookies.email,
+        firstName: cookies.firstName,
+        lastName: cookies.lastName,
+        preferredUsername: cookies.preferredUsername,
+        name: cookies.name,
+        picture: cookies.picture,
+        givenName: cookies.givenName,
+        familyName: cookies.familyName,
+        middleName: cookies.middleName,
+        nickname: cookies.nickname,
+        profile: cookies.profile,
+        website: cookies.we
+      }}
+  
     >
       <Component {...pageProps} />
     </SSRKeycloakProvider>
@@ -30,7 +64,7 @@ function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
   )
 
 }
-function parseCookies(req?: IncomingMessage) {
+function parseCookies(req?: IncomingMessage) { // IncomingMessage for 
   if (req) {
     return cookie.parse(req.headers.cookie || '')
   }
